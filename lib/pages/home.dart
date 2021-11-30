0fcc0f23a2027c272f7397a78c36ecfcd332fc01
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 19:07:19 +0200

    form validation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:ch6_form_validation/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,13 +9,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GlobalKey<FormState> _formStateKey = GlobalKey<FormState>();
+
+  // Order to save
+  final Order _order = Order();
+
+  String? _validateItemRequired(String value) {
+    return value.isEmpty ? 'Item Required' : null;
+  }
+
+  String? _validateItemCount(String value) {
+    // Check if the value is not null and convert to integer
+    int? _valueAsInteger = value.isEmpty ? 0 : int.tryParse(value);
+    return _valueAsInteger == 0 ? 'At least one Item is required' : null;
+  }
+
+  void _submitOrder() {
+    if (_formStateKey.currentState!.validate()) {
+      _formStateKey.currentState!.save();
+      print('Order Item: ${_order.item}');
+      print('Order Quantity: ${_order.quantity}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Form(
+              key: _formStateKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Espresso',
+                        labelText: 'Item',
+                      ),
+                      validator: (value) => _validateItemRequired(value!),
+                      onSaved: (value) => _order.item = value,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: '3',
+                        labelText: 'Quantity',
+                      ),
+                      validator: (value) => _validateItemCount(value!),
+                      onSaved: (value) =>
+                          _order.quantity = int.tryParse(value!),
+                    ),
+                    const Divider(height: 32.0),
+                    MaterialButton(
+                      onPressed: _submitOrder,
+                      child: const Text('Save'),
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
